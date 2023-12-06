6000174782c7e5453a231615d0ac2f093e77ea15
Oriana Campos <orianacampos99@gmail.com>
2023-12-06 12:19:02 -0300

    preentrega 1,simulador de cuotas

---
+++ index.js
@@ -0,0 +1,25 @@
+
+//Hice un simulador de calculos en pagos en cuotas.... 
+
+let valorProducto = parseFloat (prompt ("Ingrese el valor del producto a comprar (sin el signo de $)"))
+
+if (!isNaN(valorProducto)) {
+    let cantidadDeCuotas = parseInt (prompt ("Quiere abonar $" + valorProducto + " en 1, 2, 3, 6 o 12 cuotas?"))
+
+    let cuotaAPagar = valorProducto/cantidadDeCuotas
+
+    while (cantidadDeCuotas) {
+
+         if (cantidadDeCuotas == "1" || cantidadDeCuotas == "2" || cantidadDeCuotas == "3" || cantidadDeCuotas == "6" || cantidadDeCuotas == "12" ) {
+         alert ("Perfecto, calculemos el valor a abonar en cada cuota")
+         } else {alert ("No selecciono una cuota disponible")
+            cantidadDeCuotas=false
+            break }
+
+        if (cantidadDeCuotas=="1"){alert ("Abonas " + cantidadDeCuotas + " pago de $ " + cuotaAPagar + " total.")} 
+          else {alert ("Abonas " + cantidadDeCuotas + " pagos de $ " + cuotaAPagar + " mensual.")
+            break}
+                         }
+    }
+
+else {alert ("No ingreso un numero")}
